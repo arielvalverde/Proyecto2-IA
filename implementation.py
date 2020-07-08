73a2222fa90e9d5c6bf558711f6c9dac3e8793bf
Ariel Valverde Ramirez <38445003+arielvalverde@users.noreply.github.com>
2020-07-08 17:01:57 -0600

    Nueva referencia

--- implementation.py
+++ implementation.py
@@ -118,30 +118,6 @@ class PriorityQueue:
     def get(self):
         return heapq.heappop(self.elements)[1]
 
-def dijkstra_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-    
-    while not frontier.empty():
-        current = frontier.get()
-        
-        if current == goal:
-            break
-        
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost
-                frontier.put(next, priority)
-                came_from[next] = current
-    
-    return came_from, cost_so_far
-
 # thanks to @m1sp <Jaiden Mispy> for this simpler version of
 # reconstruct_path that doesn't have duplicate entries
 
@@ -161,7 +137,7 @@ def heuristic(a, b):
     return abs(x1 - x2) + abs(y1 - y2)
 
 def a_star_search():
-    start, goal = (1, 4), (7, 8)
+    start, goal = (1, 7), (5, 6)
     graph = diagram4
     frontier = PriorityQueue()
     frontier.put(start, 0)
